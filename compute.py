6b5fad25709f7e1b91f16c1f961b4b7dc7a061b9
Emaan Ahmed <emaan.aka@gmail.com>
2024-04-15 22:56:00 -0400

    removed loop for only single computation

--- compute.py
+++ compute.py
@@ -2,9 +2,9 @@ from mpyc.runtime import mpc
 
 
 # With 3 parties:
-#   python compute.py -M3 -I0 --no-log
-#   python compute.py -M3 -I1 --no-log
-#   python compute.py -M3 -I2 --no-log
+#   python3 compute.py -M3 -I0 --no-log
+#   python3 compute.py -M3 -I1 --no-log
+#   python3 compute.py -M3 -I2 --no-log
 async def main():
     secint = mpc.SecInt(16)
     input('Please tell us who are you?\n')
@@ -14,22 +14,22 @@ async def main():
     print('Please input the most recent one transaction that happened between you and the customer')
     """Asks about the most recent 3 transactions"""
     weighted_scores = []
-    for _ in range(3):
-        months_ago = int(input('How many months ago did this transaction happen? '))
-        amount = int(input('Enter the amount of the transaction in dollars: '))
+    
+    months_ago = int(input('How many months ago did this transaction happen? '))
+    amount = int(input('Enter the amount of the transaction in dollars: '))
 
-        month_weight = get_weight_by_months(months_ago)
-        amount_weight = get_weight_by_amount(amount)
+    month_weight = get_weight_by_months(months_ago)
+    amount_weight = get_weight_by_amount(amount)
 
-        weighted_score = int(month_weight * amount_weight * 100)
-        weighted_scores.append(weighted_score)
-        print(f"The approximate scores is: {weighted_score}")
+    weighted_score = int(month_weight * amount_weight * 100)
+    weighted_scores.append(weighted_score)
+    print(f"The approximate scores is: {weighted_score}")
 
-        all_scores = mpc.input(secint(weighted_score))
-        m = len(mpc.parties)
-        sum_scores = sum(all_scores)
+    all_scores = mpc.input(secint(weighted_score))
+    m = len(mpc.parties)
+    sum_scores = sum(all_scores)
 
-        print('Average score of recent transaction from all agencies is:', await mpc.output(sum_scores) / m)
+    print('Average score of recent transaction from all agencies is:', await mpc.output(sum_scores) / m)
 
     await mpc.shutdown()
 
