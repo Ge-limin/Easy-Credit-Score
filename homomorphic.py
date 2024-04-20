e249c1c3c931268305b88595676d58f8accacb9a
liminge <liminge@andrew.cmu.edu>
2024-04-20 15:52:37 -0400

    a little bit more clear

--- homomorphic.py
+++ homomorphic.py
@@ -1,6 +1,7 @@
-
-
-# todo
-def encrypt(original_data, encryption_method):
+def encrypt(original_data, some_args):
     """Each agency will call this function to encrypt the data before sending it to the MPC server"""
-    return encryption_method(original_data)
+    return some_function(original_data, some_args)
+
+
+def some_function(original_data, some_args):
+    return None
